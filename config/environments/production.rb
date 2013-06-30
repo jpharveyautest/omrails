39be4c4a1f24f293f296d2d71d5f2f679c0b2d2d
JP <jpharvey@remedyone.net>
2013-06-30 07:10:35 -0700

    Add user model with Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Omrails::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.default_url_options = { :host => 'protected-ocean-8419.herokuapp.com' }
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
